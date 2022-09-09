e95b1f98ccd70c62e92dece16b40c830f49d96e9
Raka Fantino <rakafantino@gmail.com>
2022-09-09 13:48:26 +0700

    Commit Next JS My News App Next JS

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,24 +7,16 @@ function Navbar() {
     <>
       <div className="bg-indigo-500 drop-shadow-lg">
         <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
-          <header className="flex gap-11 items-center py-8 px-20 md:py-8 ">
+          <header className="flex gap-11 items-center justify-center py-8 px-20 md:py-8 ">
             {/* <!-- logo - start --> */}
 
             <Link href="/" aria-label="logo">
-              <a className="inline-flex items-center text-white text-xl font-bold gap-2.5">
+              <a className="inline-flex items-center text-white text-2xl font-bold gap-2.5">
                 <NewspaperIcon className="h-6 w-6" />
                 News Update
               </a>
             </Link>
             {/* <!-- logo - end --> */}
-
-            {/* <!-- nav - start --> */}
-            <nav className="flex gap-12 text-neutral-200 hover:text-neutral-400 active:text-neutral-700 text-lg font-semibold transition duration-100">
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </nav>
-            {/* <!-- nav - end --> */}
           </header>
         </div>
       </div>
